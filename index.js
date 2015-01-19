676f5c4e27b2a21f22d0b76f3bcb532668622285
Gustav Nikolaj Olsen <gno@one.com>
2015-01-19 21:45:33 +0100

    move relayEvents to lib and add mocha.opts

--- index.js
+++ index.js
@@ -1,30 +1,3 @@
-var EventEmitter = require('events').EventEmitter;
-
-function relayEvents(event, destination, source) {
-    if (typeof event === 'string') {
-        if (event === '*') {
-            var sourceEmit = source.emit;
-            source.emit = function (eventName, eventInfo) {
-                destination.emit(eventName, eventInfo);
-                return sourceEmit(eventName, eventInfo);
-            };
-        } else {
-            source.on(event, function (e) {
-                destination.emit(event, e);
-            });
-        }
-    } else if (event instanceof RegExp) {
-        var sourceEmit = source.emit;
-        source.emit = function (eventName, eventInfo) {
-            if (event.test(eventName)) {
-                destination.emit(eventName, eventInfo);
-            }
-            return sourceEmit(eventName, eventInfo);
-        };
-    }
-}
-
-
 module.exports = {
-    relayEvents: relayEvents
+    relayEvents: require('./lib/relayEvents')
 };
---
+++ test/lib/relayEvents.spec.js
@@ -0,0 +1,70 @@
+var sinon = require('sinon');
+var expect = require('unexpected')
+    .installPlugin(require('unexpected-sinon'));
+var relayEvents = require('../../lib/relayEvents');
+var EventEmitter = require('events').EventEmitter;
+
+describe('relayEvents', function () {
+    it('should be a function', function () {
+        expect(relayEvents, 'to be a function');
+    });
+    it('should relay events from one EventEmitter to another', function (done) {
+        var listenerSpy = sinon.spy();
+        var anotherListenerSpy = sinon.spy();
+        var firstEmitter = new EventEmitter();
+        var secondEmitter = new EventEmitter();
+
+        firstEmitter.on('someEvent', listenerSpy);
+
+        relayEvents('someEvent', firstEmitter, secondEmitter);
+
+        secondEmitter.emit('someEvent', 'someMessage');
+        secondEmitter.emit('anotherEvent', 'anotherMessage');
+
+        setTimeout(function () {
+            expect(listenerSpy, 'was called with', 'someMessage');
+            expect(anotherListenerSpy, 'was not called');
+            done();
+        }, 1);
+    });
+    it('should relay matched events from one EventEmitter to another', function (done) {
+        var someListenerSpy = sinon.spy();
+        var anotherListenerSpy = sinon.spy();
+        var negativeListenerSpy = sinon.spy();
+        var firstEmitter = new EventEmitter();
+        var secondEmitter = new EventEmitter();
+
+        firstEmitter.on('someEvent', someListenerSpy);
+        firstEmitter.on('anotherEvent', anotherListenerSpy);
+        firstEmitter.on('negativeEvent', negativeListenerSpy);
+
+        relayEvents(/(some|another)Event/, firstEmitter, secondEmitter);
+
+        secondEmitter.emit('someEvent', 'someMessage');
+        secondEmitter.emit('anotherEvent', 'anotherMessage');
+        secondEmitter.emit('negativeEvent', 'negativeMessage');
+
+        setTimeout(function () {
+            expect(someListenerSpy, 'was called with', 'someMessage');
+            expect(anotherListenerSpy, 'was called with', 'anotherMessage');
+            expect(negativeListenerSpy, 'was not called');
+            done();
+        }, 1);
+    });
+    it('should relay everything', function (done) {
+        var listenerSpy = sinon.spy();
+        var firstEmitter = new EventEmitter();
+        var secondEmitter = new EventEmitter();
+
+        firstEmitter.on('someEvent', listenerSpy);
+
+        relayEvents('*', firstEmitter, secondEmitter);
+
+        secondEmitter.emit('someEvent', 'someMessage');
+
+        setTimeout(function () {
+            expect(listenerSpy, 'was called with', 'someMessage');
+            done();
+        }, 1);
+    });
+});
