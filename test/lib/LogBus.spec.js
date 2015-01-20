a7ac301e5b2c6bb3cec5e9aaf4bce9e2092d6f59
Gustav Nikolaj Olsen <gno@one.com>
2015-01-20 12:40:58 +0100

    add two skipped tests

--- test/lib/LogBus.spec.js
+++ test/lib/LogBus.spec.js
@@ -21,4 +21,35 @@ describe('LogBus', function () {
             done();
         }, 1);
     });
+    it.skip('should allow you to read a stream of events from a query', function () {
+        var logBus = new LogBus();
+
+        var stream = logBus.streamQuery({ query: 'foobar' });
+
+        // buffer events from stream
+
+        logBus.emit('log', 'event that match the query');
+        logBus.emit('log', 'event that match the query');
+        logBus.emit('log', 'event that match the query');
+
+        expect('buffered result', 'to equal', 'expected result, stringified event as json one per line');
+    });
+    it.skip('map non log events to log', function () {
+        var logBus = new LogBus();
+        var subscribeSpy = sinon.spy();
+
+        logBus.subscribe({ type: 'log' }, subscribeSpy);
+
+        logBus.emit('log', 'foo');
+        logBus.emit('metric', 'bar');
+        logBus.emit('log', 'qux');
+
+        setTimeout(function () {
+            expect(subscribeSpy, 'was called thrice');
+            expect(subscribeSpy, 'was called with', { type: 'log', message: 'foo' });
+            expect(subscribeSpy, 'was called with', { type: 'metric', message: 'bar' });
+            expect(subscribeSpy, 'was called with', { type: 'log', message: 'qux' });
+            done();
+        }, 1);
+    });
 });
