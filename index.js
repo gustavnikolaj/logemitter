8f14cde7bed64b840a9377b97455bba8dd1ab60d
Gustav Nikolaj Olsen <gno@one.com>
2015-01-23 11:14:08 +0100

    export LogBus directly

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-module.exports = {
-    relayEvents: require('./lib/relayEvents')
-};
+var LogBus = require('./lib/LogBus');
+
+LogBus.relayEvents = require('./lib/relayEvents');
+LogBus.augmentFunctionWithEventEmitter = require('./lib/augmentFunctionWithEventEmitter');
+LogBus.augmentObject = require('./lib/augmentObject');
+
+module.exports = LogBus;
