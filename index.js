6e6561253d3d5a9d9c1613dce315aacc301d861a
Gustav Nikolaj Olsen <gno@one.com>
2015-01-27 12:07:04 +0100

    add logemitter class

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ var LogBus = require('./lib/LogBus');
 LogBus.relayEvents = require('./lib/relayEvents');
 LogBus.augmentFunctionWithEventEmitter = require('./lib/augmentFunctionWithEventEmitter');
 LogBus.augmentObject = require('./lib/augmentObject');
+LogBus.LogEmitter = require('./lib/LogEmitter');
 
 module.exports = LogBus;
