da313060fbc807a2345fa2747d545a2da0f60df2
Gustav Nikolaj Olsen <gno@one.com>
2015-02-06 13:46:49 +0100

    remove augmentObject method
    Replaced by LogEmitter object. Takes care of the same...
    
    Almost - we might need augmentObject later if you were to extend
    an object which was already instantiated and was an instance of
    EventEmitter. I just can't see how we should need that at this
    point in time...

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var LogBus = require('./lib/LogBus');
 
 LogBus.relayEvents = require('./lib/relayEvents');
 LogBus.augmentFunctionWithEventEmitter = require('./lib/augmentFunctionWithEventEmitter');
-LogBus.augmentObject = require('./lib/augmentObject');
 LogBus.LogEmitter = require('./lib/LogEmitter');
 LogBus.HttpEventAggregator = require('./lib/HttpEventAggregator');
 
