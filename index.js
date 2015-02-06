88734110e2ebd7602e0afaa9a4a12a1820c0bef4
Gustav Nikolaj Olsen <gno@one.com>
2015-02-06 13:45:40 +0100

    export HttpEventAggregator

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ LogBus.relayEvents = require('./lib/relayEvents');
 LogBus.augmentFunctionWithEventEmitter = require('./lib/augmentFunctionWithEventEmitter');
 LogBus.augmentObject = require('./lib/augmentObject');
 LogBus.LogEmitter = require('./lib/LogEmitter');
+LogBus.HttpEventAggregator = require('./lib/HttpEventAggregator');
 
 module.exports = LogBus;
