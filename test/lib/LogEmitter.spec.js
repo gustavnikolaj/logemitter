f219eadd7cab5f73673a774336698eff0152ea74
Gustav Nikolaj Olsen <gno@one.com>
2015-02-16 14:45:27 +0100

    add logBaseObject to LogEmitter

--- test/lib/LogEmitter.spec.js
+++ test/lib/LogEmitter.spec.js
@@ -27,4 +27,46 @@ describe('lib/LogEmitter', function () {
             });
         });
     });
+    describe('baseLogObject', function () {
+        it('should add basic properties to the emitted object', function (done) {
+            var logEmitter = new LogEmitter();
+            logEmitter.on('log', function (e) {
+                expect(e, 'to satisfy', {
+                    type: 'log',
+                    message: 'a log message'
+                });
+                done();
+            });
+            logEmitter.emit('log', { message: 'a log message' });
+        });
+        it('should be able to extend the baseLogObject', function (done) {
+            var logEmitter = new LogEmitter();
+            logEmitter.on('log', function (e) {
+                expect(e, 'to satisfy', {
+                    message: 'a log message',
+                    extraAttribute: true
+                });
+                done();
+            });
+            logEmitter.logBaseObject = {
+                extraAttribute: true
+            };
+            logEmitter.emit('log', { message: 'a log message' });
+        });
+        it('should keep the defaults when adding extra attributes', function (done) {
+            var logEmitter = new LogEmitter();
+            logEmitter.on('log', function (e) {
+                expect(e, 'to satisfy', {
+                    type: 'log',
+                    message: 'a log message',
+                    extraAttribute: true
+                });
+                done();
+            });
+            logEmitter.logBaseObject = {
+                extraAttribute: true
+            };
+            logEmitter.emit('log', { message: 'a log message' });
+        });
+    });
 });
