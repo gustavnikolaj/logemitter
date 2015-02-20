07b855283abd382e649d8f423cd71659e281edad
Gustav Nikolaj Olsen <gno@one.com>
2015-02-20 15:28:28 +0100

    remove deprecated function augmentFunctionWithEventEmitter

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var LogBus = require('./lib/LogBus');
 
 LogBus.relayLogEvents = require('./lib/relayLogEvents');
-LogBus.augmentFunctionWithEventEmitter = require('./lib/augmentFunctionWithEventEmitter');
 LogBus.LogEmitter = require('./lib/LogEmitter');
 LogBus.HttpEventAggregator = require('./lib/HttpEventAggregator');
 
