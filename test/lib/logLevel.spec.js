aacc221f86fa651ccb6b27be147709e1adcba419
Gustav Nikolaj Olsen <gno@one.com>
2015-02-20 15:56:57 +0100

    add logLevel.fromString method

--- test/lib/logLevel.spec.js
+++ test/lib/logLevel.spec.js
@@ -15,7 +15,35 @@ describe('lib/loglevel', function () {
             DEBUG: 7
         });
     });
-    it('should have convinient aliases', function () {
-        expect(logLevel, 'to have property', 'INFO', 7);
+    it('should be able to return a severity level from a string', function () {
+        expect(logLevel.fromString('notice'), 'to be', 5);
+    });
+    describe('.fromString(...)', function () {
+        [
+            ['EMERGENCY', 0],
+            ['emergency', 0],
+            ['ALERT', 1],
+            ['alert', 1],
+            ['CRITICAL', 2],
+            ['critical', 2],
+            ['ERROR', 3],
+            ['error', 3],
+            ['WARNING', 4],
+            ['warning', 4],
+            ['NOTICE', 5],
+            ['notice', 5],
+            ['INFORMATIONAL', 6],
+            ['informational', 6],
+            ['INFO', 6],
+            ['info', 6],
+            ['DEBUG', 7],
+            ['debug', 7],
+        ].forEach(function (test) {
+            var testString = test[0];
+            var testNumber = test[1];
+            it(testString + ' => ' + testNumber, function () {
+                expect(logLevel.fromString(testString), 'to be', testNumber);
+            });
+        });
     });
 });
