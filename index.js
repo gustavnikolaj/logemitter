271433187b410ed33a0133231344844ab62d8b3f
Gustav Nikolaj Olsen <gno@one.com>
2015-06-26 16:32:13 +0200

    remove everything but the LogEmitter

--- index.js
+++
@@ -1,7 +0,0 @@
-var LogBus = require('./lib/LogBus');
-
-LogBus.relayLogEvents = require('./lib/relayLogEvents');
-LogBus.LogEmitter = require('./lib/LogEmitter');
-LogBus.HttpEventAggregator = require('./lib/HttpEventAggregator');
-
-module.exports = LogBus;
