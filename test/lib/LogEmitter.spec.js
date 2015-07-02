ce03806437e70c96f3f041745af2d3a68ff1b4b2
Gustav Nikolaj Olsen <gno@one.com>
2015-07-02 09:58:42 +0200

    make tests sync

--- test/lib/LogEmitter.spec.js
+++ test/lib/LogEmitter.spec.js
@@ -82,32 +82,30 @@ describe('lib/LogEmitter', function () {
         });
     });
     describe('baseLogObject', function () {
-        it('should add basic properties to the emitted object', function (done) {
+        it('should add basic properties to the emitted object', function () {
             var logEmitter = new LogEmitter();
             logEmitter.on('log', function (e) {
                 expect(e, 'to satisfy', {
                     type: 'log',
                     message: 'a log message'
                 });
-                done();
             });
             logEmitter.emit('log', { message: 'a log message' });
         });
-        it('should be able to extend the baseLogObject', function (done) {
+        it('should be able to extend the baseLogObject', function () {
             var logEmitter = new LogEmitter();
             logEmitter.on('log', function (e) {
                 expect(e, 'to satisfy', {
                     message: 'a log message',
                     extraAttribute: true
                 });
-                done();
             });
             logEmitter.logBaseObject = {
                 extraAttribute: true
             };
             logEmitter.emit('log', { message: 'a log message' });
         });
-        it('should keep the defaults when adding extra attributes', function (done) {
+        it('should keep the defaults when adding extra attributes', function () {
             var logEmitter = new LogEmitter();
             logEmitter.on('log', function (e) {
                 expect(e, 'to satisfy', {
@@ -115,7 +113,6 @@ describe('lib/LogEmitter', function () {
                     message: 'a log message',
                     extraAttribute: true
                 });
-                done();
             });
             logEmitter.logBaseObject = {
                 extraAttribute: true
